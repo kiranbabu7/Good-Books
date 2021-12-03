4033ac3c5462ac1e91ee9db6fe9995cece394e48
Kiran Babu <kiranbabu7675@gmail.com>
2021-12-03 21:17:32 +0530

    feat: Genre buttons now working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [currentGenre, setCurrentGenre] = useState("Biryani");
+  function genreClickHandler(event) {
+    let newGenre = event.target.innerText;
+    setCurrentGenre(newGenre);
+  }
+
+  const foodCollection = {
+    Biryani: [
+      { name: "Dum Biryani", rating: "4/5" },
+      { name: "Kalmi Biryani", rating: "5 / 5" },
+    ],
+    Pizza: [
+      { name: "Chicken Supreme", rating: "3.5/5" },
+      { name: "Tandoori Paneer", rating: "4/5" },
+    ],
+    Chicken: [
+      { name: "Chicken Lolipop", rating: "5/5" },
+      { name: "Chicken Tandoori", rating: "4/5" },
+    ],
+  };
+  const genres = Object.keys(foodCollection);
+  console.log(foodCollection[currentGenre]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Junk Food</h1>
+      <p>Select a Genre to get Started</p>
+      <div className="buttons">
+        {genres.map((genre) => {
+          return (
+            <button key={genre} onClick={genreClickHandler}>
+              {genre}
+            </button>
+          );
+        })}
+      </div>
+      <div className="container">
+        <ul>
+          {foodCollection[currentGenre].map((item) => {
+            return (
+              <li>
+                <div>Name: {item.name}</div>
+                <div>Rating: {item.rating}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
